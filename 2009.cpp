3d3ca40052f69f84088b1dbdc0e867f86dc34969
lvting232 <50477450+lvting232@users.noreply.github.com>
2019-05-10 16:19:22 +0800

    Update 2009.cpp
    Eidt function initArray

--- 2009.cpp
+++ 2009.cpp
@@ -4,23 +4,23 @@
 #include <time.h>
 #include <stdlib.h>
 
-//void initArray(int a[], int n, int r){
-//	int k; long now;
-//	srand(time(NULL)); /* 用时间初始化随机数发生函数的初态*/
-//	for(k = 0; k < n; k++) 
-//		/* 产生n个0..r-1以内的随机数 */
-//		a[k] = rand() % r; /* 调用随机函数 */
-//}
+void initArray(int a[], int n, int r){
+	int k; long now;
+	srand(time(NULL)); /* 用时间初始化随机数发生函数的初态*/
+	for(k = 0; k < n; k++) 
+		/* 产生n个0..r-1以内的随机数 */
+		a[k] = rand() % r; /* 调用随机函数 */
+}
 
 //通过随机数初始化数据
-void initArray(int a[],int n,int r)
-{
-	srand(time(NULL));
-	for(int k =0;k<n; k++)
-	{
-		a[k]= rand()%r;
-	}
-}
+//void initArray(int a[],int n,int r)
+//{
+//	srand(time(NULL));
+//	for(int k =0;k<n; k++)
+//	{
+//		a[k]= rand()%r;
+//	}
+//}
 void writeArray(int a[], int n, int c){
 	for(int k = 0; k < n; k++){
 		if(k && k % c == 0)//一行输出c个整数，k是c的倍数的时候 换行
